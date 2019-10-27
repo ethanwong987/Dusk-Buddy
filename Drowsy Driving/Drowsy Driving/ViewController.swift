280257e3cbbfc9fe5cfecb1eb7392afe6a6a08b0
Ethan Wong <ethanwong987@gmail.com>
2019-10-27 08:47:56 -0700

    fsd

--- Drowsy Driving/Drowsy Driving/ViewController.swift
+++ Drowsy Driving/Drowsy Driving/ViewController.swift
@@ -16,31 +16,58 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var labelView: UIView!
     @IBOutlet weak var faceLabel: UILabel!
+    @IBOutlet weak var overlayView: UIView!
+    @IBOutlet weak var screen1: UIImageView!
+    @IBOutlet weak var screen2: UIImageView!
+    @IBAction func swipeButton(_ sender: TGFlingActionButton) {
+    }
+    
+    var awakeText: UILabel!
     var analysis = ""
     var timer = [String]()
+    var button: UIButton!
     private var audioPlayer: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        // 1
+        //createButton()
         labelView.layer.cornerRadius = 10
-     
         sceneView.delegate = self
         sceneView.showsStatistics = true
-     
-        // 2
+        constraints()
+        screen2.isHidden = true
+        
+        
         guard ARFaceTrackingConfiguration.isSupported else {
             fatalError("Face tracking is not supported on this device")
         }
         
         
-        let sound = Bundle.main.path(forResource: "DJ Airhorn Sound Effect", ofType: "mp3")
+        let sound = Bundle.main.path(forResource: "alarm", ofType: "mp3")
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
         }
         catch {
             print(error)
         }
+    }
+
+    func createButton() {
+        button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        button.backgroundColor = .green
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    @IBAction func flingActionCallback(_ sender: TGFlingActionButton) {
+        audioPlayer.stop()
+        self.screen1.isHidden = false
+        self.screen2.isHidden = true
+    }
+    @objc func buttonAction(sender: UIButton!) {
+        audioPlayer.stop()
+        self.screen1.isHidden = false
+        self.screen2.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -58,6 +85,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    func constraints() {
+        screen1.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        screen1.heightAnchor.constraint(equalToConstant: 180).isActive = true
+        screen1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        screen1.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 28).isActive = true
     }
 
     // MARK: - ARSCNViewDelegate
@@ -78,6 +111,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             DispatchQueue.main.async {
                 self.faceLabel.text = self.analysis
+              
             }
             
         }
@@ -88,15 +122,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.analysis = ""
      
         if(leftEye?.decimalValue ?? 0.0 > 0.8) && (rightEye?.decimalValue ?? 0.0 > 0.8){
-            self.analysis += "Your eyes are closed."
-                
-            self.timer.append("penis")
-            if self.timer.count > 70{
+            self.analysis += "AWAKE"
+            self.timer.append("closed")
+            print("closed")
+            if self.timer.count > 30{
                 audioPlayer.play()
-            //AudioServicesPlaySystemSound(SystemSoundID(1005))
+                DispatchQueue.main.async {
+                    self.screen1.isHidden = true
+                    self.screen2.isHidden = false
+                }
+                
             }
         } else {
             self.timer = []
+            DispatchQueue.main.async {
+                self.audioPlayer.stop()
+                self.screen1.isHidden = false
+                self.screen2.isHidden = true
+            }
+            
         }
     }
 }
